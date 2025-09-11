bb665b0ca9ae5038ae989ab696d7253c18366add
Inmemorialake <inmemorialake2@gmail.com>
2025-09-10 21:45:58 -0500

    Some of the systems archives has changed

--- .scala-build/FPFC_d5c0a6989e/src_generated/main/workshop_1/test.scala
+++ .scala-build/FPFC_d5c0a6989e/src_generated/main/workshop_1/test.scala
@@ -7,35 +7,31 @@ def scriptPath = """workshop_1/test.sc"""
 /*<script>*/
 import FuncionesRecursivas._
 
-// ----- Pruebas de maxLin -----
-maxLin(List(5))              // 5
-maxLin(List(3,2,9,1))        // 9
-maxLin(List(10,10,10))       // 10
-maxLin(List(1,2,3,4,5))      // 5
-maxLin(List(100,2,99,3))     // 100
+maxLin(List(5))            
+maxLin(List(3,2,9,1))  
+maxLin(List(10,10,10))   
+maxLin(List(1,2,3,4,5))   
+maxLin(List(100,2,99,3))    
 
-// ----- Pruebas de maxIt -----
-maxIt(List(5))               // 5
-maxIt(List(3,2,9,1))         // 9
-maxIt(List(10,10,10))        // 10
-maxIt(List(1,2,3,4,5))       // 5
-maxIt(List(100,2,99,3))      // 100
+maxIt(List(5))            
+maxIt(List(3,2,9,1))      
+maxIt(List(10,10,10))     
+maxIt(List(1,2,3,4,5))    
+maxIt(List(100,2,99,3))      
 
-// ----- Pruebas de movsTorresHanoi -----
-movsTorresHanoi(1)           // 1
-movsTorresHanoi(2)           // 3
-movsTorresHanoi(3)           // 7
-movsTorresHanoi(4)           // 15
-movsTorresHanoi(5)           // 31
-movsTorresHanoi(64)          // número enorme
+movsTorresHanoi(1)
+movsTorresHanoi(2)
+movsTorresHanoi(3)
+movsTorresHanoi(4)
+movsTorresHanoi(5)
+movsTorresHanoi(64)
 val siglo: BigInt = BigInt(60) * BigInt(60) * BigInt(24) * BigInt(365) * BigInt(100)
-movsTorresHanoi(64) / siglo  // comparación con siglos
+movsTorresHanoi(64) / siglo
 
-// ----- Pruebas de torresHanoi -----
-torresHanoi(1,1,2,3)         // List((1,3))
-torresHanoi(2,1,2,3)         // List((1,2), (1,3), (2,3))
-torresHanoi(3,1,2,3)         // movimientos esperados para 3 discos
-torresHanoi(4,1,2,3)         // movimientos esperados para 4 discos
+torresHanoi(1,1,2,3)
+torresHanoi(2,1,2,3)
+torresHanoi(3,1,2,3)
+torresHanoi(4,1,2,3)
 
 /*</script>*/ /*<generated>*//*</generated>*/
 }
